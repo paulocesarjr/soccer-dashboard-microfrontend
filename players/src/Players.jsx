1d68d89054ec59c8fcebe1ef3d287e2c47de2759
Paulo Cesar Jr <paulocesarrosajr@gmail.com>
2025-03-06 09:48:29 -0300

    feat: setup federation to load micro frontends

---
+++ players/src/Players.jsx
@@ -0,0 +1,5 @@
+function Players() {
+  return <h1>Players</h1>;
+}
+
+export default Players;
--- players/vite.config.js
+++ players/vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "players",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Players": "./src/Players",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  preview: {
+    host: "localhost",
+    port: 5002,
+    strictPort: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+});
